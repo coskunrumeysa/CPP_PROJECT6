401c9d888b415166d918ab9d3767007668a69edd
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-07-22 15:24:45 +0300

    Add files via upload

---
+++ Project9_cplusplus.cpp
@@ -0,0 +1,32 @@
+﻿// Beni Yaratan Elbet Yolumu Gösterir .Şuara7 /8
+//Bu program bir küpün alanını,çevresini ve hacmini bulmak için yazılmıştır.
+// This program written for calcualte area,perimeter and volume of a cube.
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int side;
+	int area;
+	int perimeter;
+	int volume;
+	
+	cout << "Please enter a side of cube:" << endl;
+	cin >> side;
+
+	area = (side*side*6);
+	perimeter = (12 * side);
+	volume = (side * side * side);
+
+	cout << endl;
+
+	cout << "Area of cube:\t"<< area << endl;
+	cout << "Perimeter of cube:\t"<< perimeter << endl;
+	cout << "Volume of cube:\t"<< volume << endl;
+
+	system("PAUSE");
+	return 0;
+}
+
